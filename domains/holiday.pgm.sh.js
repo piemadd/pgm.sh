26460a6dd390e6d335b2f3cd2c0cebca207ad673
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2023-11-17 23:30:36 -0600

    added holiday tracker

---
+++ domains/holiday.pgm.sh.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'CTA Holiday Vehicle Tracker',
+  domain: 'pgm.sh',
+  subdomain: 'holiday',
+  owner: {
+    repo: 'https://github.com/piemadd/cta-holiday-vehicle-tracker',
+  },
+  record: {
+    CNAME: 'cta-holiday-vehicle-tracker.pages.dev',
+  },
+  proxy: true,
+})
