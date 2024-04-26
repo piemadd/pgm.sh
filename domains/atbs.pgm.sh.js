1e5d1ea7159aecf48af16588c7e42f1a56088792
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2024-04-26 00:05:40 -0500

    adding all the boats api

---
+++ domains/atbs.pgm.sh.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'All the Boats Server',
+  domain: 'pgm.sh',
+  subdomain: 'atbs',
+  owner: {
+    repo: 'https://github.com/piemadd/all-the-boats',
+  },
+  record: {
+    A: '45.63.8.53',
+  },
+  proxy: true,
+})
