8f5bfa698867a842fa18ce574457a1ebd0ace80c
Piero Maddaleni <piero@piemadd.com>
2024-07-25 12:26:03 -0500

    added fxtiktok subdomains

---
+++ domains/dtiktok.pgm.sh.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'Tiktok Embed',
+  domain: 'pgm.sh',
+  subdomain: 'dtiktok',
+  owner: {
+    repo: 'https://github.com/piemadd/fxTikTok',
+  },
+  record: {
+    CNAME: 'piero.workers.dev',
+  },
+  proxy: true,
+})
---
+++ domains/tiktok.pgm.sh.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'Tiktok Embed',
+  domain: 'pgm.sh',
+  subdomain: 'tiktok',
+  owner: {
+    repo: 'https://github.com/piemadd/fxTikTok',
+  },
+  record: {
+    CNAME: 'piero.workers.dev',
+  },
+  proxy: true,
+})
