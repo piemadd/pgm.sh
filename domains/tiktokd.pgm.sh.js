449716d3f2ab88707c2243821314a37736e500d5
Piero Maddaleni <piero@piemadd.com>
2024-07-25 12:36:02 -0500

    added tiktok direct

---
+++ domains/tiktokd.pgm.sh.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'Tiktok Embed Direct',
+  domain: 'pgm.sh',
+  subdomain: 'tiktokd',
+  owner: {
+    repo: 'https://github.com/piemadd/fxTikTok',
+  },
+  record: {
+    CNAME: 'piero.workers.dev',
+  },
+  proxy: true,
+})
